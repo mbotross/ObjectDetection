fad4b18836d33175563c030f4e6b3ff64924e5b2
mbotross <mbotross@uci.edu>
2019-11-16 19:37:05 -0800

    shape detection and contour lines

--- obj.py
+++ obj.py
@@ -9,17 +9,17 @@ import numpy as np
 
 def ColRange():
 #Define Red
-    lower_red = np.array([0, 120, 70], dtype=np.uint8)
+    lower_red = np.array([161, 155, 84], dtype=np.uint8)
 
-    upper_red = np.array([170, 120,70], dtype=np.uint8)
+    upper_red = np.array([215, 255,255], dtype=np.uint8)
 #    inRange(hsv, Scalar(0, 120, 70), Scalar(10, 255, 255), lower_red);
 #    inRange(hsv, Scalar(170, 120, 70), Scalar(180, 255, 255), upper_red);
 
     red = [lower_red, upper_red, 'red']
 
     #Define Blue
-    lower_blue = np.array([90, 20, 60], dtype=np.uint8)
-    upper_blue = np.array([130, 255, 180], dtype=np.uint8)
+    lower_blue = np.array([100, 150, 0], dtype=np.uint8)
+    upper_blue = np.array([140, 255, 255], dtype=np.uint8)
     blue = [lower_blue, upper_blue, 'blue']
     
     #Define Yellow
@@ -37,7 +37,7 @@ def ColRange():
     upper_green = np.array([70, 250, 250], dtype=np.uint8)
     green = [lower_green, upper_green, 'green']
 
-    colors=[blue,yellow,green,red,white]
+    colors=[blue,yellow,red,white,green]
     return colors
 
 def Color(img):
@@ -57,7 +57,7 @@ def Color(img):
 
 
 
-img=cv2.imread('objy.png',1)
+img=cv2.imread('shapes.png',1)
 Color(img)
 
     
---
+++ shape.py
@@ -0,0 +1,59 @@
+
+import numpy as np
+import obj
+import cv2
+
+img = cv2.imread('testim.png')
+hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
+colors=obj.ColRange()
+kernel=np.ones((3,3),np.uint8)
+mask1=cv2.inRange(hsv,colors[0][0],colors[0][1])
+contours2,hierarchy2=cv2.findContours(mask1,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+for color in colors:
+    mask=cv2.inRange(hsv,color[0],color[1])
+    mask=cv2.erode(mask,kernel)
+    if color[2]!="green":
+    	flag=1    
+    if((color[2]!="green" and flag==1) or (color[2]=="green" and flag==0)): 	
+    	contours,hierarchy =cv2.findContours(mask,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
+       
+        
+    
+    if(len(contours)!=0):
+        contours2=contours2+contours
+        
+    
+
+    	
+        
+        
+        """approx = cv2.approxPolyDP(cnt,0.01*cv2.arcLength(cnt,True),True)
+       # print len(approx)
+        if len(approx)==5:
+            print ("pentagon")
+            cv2.drawContours(img,[cnt],0,255,-1)
+        elif len(approx)==3:
+            print ("triangle")
+            cv2.drawContours(img,[cnt],0,(0,255,0),-1)
+        elif len(approx)==4:
+            print ("square")
+            cv2.drawContours(img,[cnt],0,(0,0,255),-1)
+        elif len(approx) == 9:
+            print ("half-circle")
+    
+            cv2.drawContours(img,[cnt],0,(255,255,0),-1)
+        elif len(approx) > 15:
+            print ("circle")
+            cv2.drawContours(img,[cnt],0,(0,255,255),-1)"""
+            
+   
+
+c = max(contours2, key = cv2.contourArea)
+x,y,w,h = cv2.boundingRect(c)
+cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
+#cv2.drawContours(img,[c],0,(0,0,0),5)       
+
+cv2.imshow('img',img)
+cv2.imwrite("changed.png",img)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
