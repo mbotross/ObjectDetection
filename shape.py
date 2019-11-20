32da2cb821b0e5b1090a87994eaf5a188d7fc407
mbotross <mbotross@uci.edu>
2019-11-19 22:55:29 -0800

    shape recognition

--- shape.py
+++ shape.py
@@ -3,7 +3,7 @@ import numpy as np
 import obj
 import cv2
 
-img = cv2.imread('testim.png')
+img = cv2.imread('testim3.png')
 hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 colors=obj.ColRange()
 kernel=np.ones((3,3),np.uint8)
@@ -51,9 +51,28 @@ for color in colors:
 c = max(contours2, key = cv2.contourArea)
 x,y,w,h = cv2.boundingRect(c)
 cv2.rectangle(img,(x,y),(x+w,y+h),(0,255,0),2)
+approx = cv2.approxPolyDP(c,0.01*cv2.arcLength(c,True),True)
+if len(approx)==5:
+    print ("pentagon")
+    cv2.drawContours(img,c,0,255,-1)
+elif len(approx)==3:
+    print ("triangle")
+    cv2.drawContours(img,c,0,(0,255,0),-1)
+elif len(approx)==4:
+    print ("square")
+    cv2.drawContours(img,c,0,(0,0,255),-1)
+elif len(approx) == 9:
+    print ("half-circle")
+
+    cv2.drawContours(img,c,0,(255,255,0),-1)
+elif len(approx) > 15:
+    print ("circle")
+    cv2.drawContours(img,c,0,(0,255,255),-1)
+
+
 #cv2.drawContours(img,[c],0,(0,0,0),5)       
 
 cv2.imshow('img',img)
-cv2.imwrite("changed.png",img)
+cv2.imwrite("changed2.png",img)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
