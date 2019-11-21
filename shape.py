97752e49fb14620a07a88c4cbd06d6cd4f0cc243
mbotross <mbotross@uci.edu>
2019-11-20 22:30:45 -0800

    added cropped images

--- shape.py
+++ shape.py
@@ -3,7 +3,7 @@ import numpy as np
 import obj
 import cv2
 
-img = cv2.imread('testim3.png')
+img = cv2.imread('testim2.png')
 hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
 colors=obj.ColRange()
 kernel=np.ones((3,3),np.uint8)
@@ -24,28 +24,7 @@ for color in colors:
         
     
 
-    	
-        
-        
-        """approx = cv2.approxPolyDP(cnt,0.01*cv2.arcLength(cnt,True),True)
-       # print len(approx)
-        if len(approx)==5:
-            print ("pentagon")
-            cv2.drawContours(img,[cnt],0,255,-1)
-        elif len(approx)==3:
-            print ("triangle")
-            cv2.drawContours(img,[cnt],0,(0,255,0),-1)
-        elif len(approx)==4:
-            print ("square")
-            cv2.drawContours(img,[cnt],0,(0,0,255),-1)
-        elif len(approx) == 9:
-            print ("half-circle")
     
-            cv2.drawContours(img,[cnt],0,(255,255,0),-1)
-        elif len(approx) > 15:
-            print ("circle")
-            cv2.drawContours(img,[cnt],0,(0,255,255),-1)"""
-            
    
 
 c = max(contours2, key = cv2.contourArea)
@@ -63,13 +42,13 @@ elif len(approx)==4:
     cv2.drawContours(img,c,0,(0,0,255),-1)
 elif len(approx) == 9:
     print ("half-circle")
-
     cv2.drawContours(img,c,0,(255,255,0),-1)
 elif len(approx) > 15:
     print ("circle")
     cv2.drawContours(img,c,0,(0,255,255),-1)
 
-
+crop=img[y:y+h, x:x+w]
+cv2.imshow("cropped",crop)
 #cv2.drawContours(img,[c],0,(0,0,0),5)       
 
 cv2.imshow('img',img)
